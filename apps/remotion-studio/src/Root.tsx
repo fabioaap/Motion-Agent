6b3ce65b986f78dee8f6cef18cef5ca602493e01
FABIO ALVES <fabiovisualmidia@gmail.com>
2026-09-16 22:25:52 -0300

    fix: use bundler friendly Remotion imports

--- apps/remotion-studio/src/Root.tsx
+++ apps/remotion-studio/src/Root.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Composition} from "remotion";
-import {demoMotionSpec} from "./demoSpec.js";
-import {MotionAgentDemo} from "./MotionAgentDemo.js";
+import {demoMotionSpec} from "./demoSpec";
+import {MotionAgentDemo} from "./MotionAgentDemo";
 
 export const RemotionRoot: React.FC = () => {
   return (
